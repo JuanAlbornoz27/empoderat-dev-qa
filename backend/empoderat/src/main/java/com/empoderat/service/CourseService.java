63f83ce3f92efac3fcb2517c0b2370a7d3249f93
Juan Albornoz <juanjosealbornoz.ing@gmail.com>
2025-06-02 17:11:25 -0500

    se añaden cambios a CourseService

--- backend/empoderat/src/main/java/com/empoderat/service/CourseService.java
+++ backend/empoderat/src/main/java/com/empoderat/service/CourseService.java
@@ -14,8 +14,8 @@ import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.multipart.MultipartFile;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
